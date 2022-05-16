a67fda32947b82636a23f2f441358a93fe8e6385
Zakariae Louqdyeme <zakarialouqdyeme@gmail.com>
2022-05-17 00:19:38 +0100

    lmonada za2id wa7iiid

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ parseInt(document.getElementById("submit").onclick= function(){
     let height=document.getElementById("height").value;
     let weight=document.getElementById("weight").value;
 
-    let m2 =parseFloat(height*height);
+    let m2 =(parseFloat(height*height)/100)/100;
     console.log(m2);
     let bmiCalcule =weight  / m2;
     console.log(weight);
